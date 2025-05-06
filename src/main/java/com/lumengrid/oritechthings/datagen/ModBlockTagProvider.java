6e19887ca3ab86f2e5575b81736d69b0b8a4ee36
DevDyna <andrearisorti2003@gmail.com>
2025-05-06 21:59:56 +0200

    datagen item->blocktags

--- src/main/java/com/lumengrid/oritechthings/datagen/ModBlockTagProvider.java
+++ src/main/java/com/lumengrid/oritechthings/datagen/ModBlockTagProvider.java
@@ -3,6 +3,7 @@ package com.lumengrid.oritechthings.datagen;
 import com.lumengrid.oritechthings.block.ModBlocks;
 import com.lumengrid.oritechthings.main.OritechThings;
 import com.lumengrid.oritechthings.util.Constants;
+import com.lumengrid.oritechthings.util.ModTags;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
@@ -22,7 +23,6 @@ public class ModBlockTagProvider extends BlockTagsProvider {
         super(output, lookupProvider, OritechThings.MOD_ID, existingFileHelper);
     }
 
-    @SuppressWarnings("null")
     @Override
     protected void addTags(HolderLookup.@NotNull Provider provider) {
 
@@ -31,5 +31,33 @@ public class ModBlockTagProvider extends BlockTagsProvider {
         }
         tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModBlocks.ACCELERATOR_SPEED_SENSOR.get());
         tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModBlocks.INFESTED_AMETHYST_BLOCK.get());
+
+        for(DeferredBlock<?> data: Constants.EFFICIENCY){
+            tag(ModTags.Blocks.TIERED_ADDON_EFFICIENCY).add(data.get());
+        }
+
+        for(DeferredBlock<?> data: Constants.SPEED){
+            tag(ModTags.Blocks.TIERED_ADDON_SPEED).add(data.get());
+        }
+
+        for(DeferredBlock<?> data: Constants.EFFICIENT){
+            tag(ModTags.Blocks.TIERED_ADDON_EFFICIENT_SPEED).add(data.get());
+        }
+
+        for(DeferredBlock<?> data: Constants.CAPACITOR){
+            tag(ModTags.Blocks.TIERED_ADDON_CAPACITOR).add(data.get());
+        }
+
+        for(DeferredBlock<?> data: Constants.ACCEPTOR){
+            tag(ModTags.Blocks.TIERED_ADDON_ACCEPTOR).add(data.get());
+        }
+
+        for(DeferredBlock<?> data: Constants.PROCESSING){
+            tag(ModTags.Blocks.TIERED_ADDON_PROCESSING).add(data.get());
+        }
+
+        tag(ModTags.Blocks.PARTICLE_ACCELERATOR).add(ModBlocks.ACCELERATOR_SPEED_SENSOR.get());
+
+
     }
 }
