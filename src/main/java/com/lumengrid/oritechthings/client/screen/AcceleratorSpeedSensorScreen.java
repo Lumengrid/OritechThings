a4576d176b375bde1b6b588a8219b4eead604008
DevDyna <andrearisorti2003@gmail.com>
2025-05-06 21:05:12 +0200

    formatted

--- src/main/java/com/lumengrid/oritechthings/client/screen/AcceleratorSpeedSensorScreen.java
+++ src/main/java/com/lumengrid/oritechthings/client/screen/AcceleratorSpeedSensorScreen.java
@@ -15,14 +15,18 @@ import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 import net.neoforged.neoforge.network.PacketDistributor;
+
 @SuppressWarnings("null")
 public class AcceleratorSpeedSensorScreen extends AbstractContainerScreen<AcceleratorSpeedSensorMenu> {
     private final Component title = Component.translatable("gui.oritechthings.particle_accelerator_speed_sensor.title");
-    public static ResourceLocation BACKGROUND = ResourceLocation.fromNamespaceAndPath(OritechThings.MOD_ID, "textures/gui/speed_sensor.png");
+    public static ResourceLocation BACKGROUND = ResourceLocation.fromNamespaceAndPath(OritechThings.MOD_ID,
+            "textures/gui/speed_sensor.png");
 
-    public AcceleratorSpeedSensorScreen(AcceleratorSpeedSensorMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
+    public AcceleratorSpeedSensorScreen(AcceleratorSpeedSensorMenu pMenu, Inventory pPlayerInventory,
+            Component pTitle) {
         super(pMenu, pPlayerInventory, pTitle);
     }
+
     @Override
     protected void renderLabels(GuiGraphics gui, int mouseX, int mouseY) {
         gui.drawString(this.font, this.title, this.titleLabelX, this.titleLabelY, 0XFFFFFF);
@@ -39,13 +43,13 @@ public class AcceleratorSpeedSensorScreen extends AbstractContainerScreen<Accele
                 Component.literal(menu.be.isCheckGreater() ? ">" : "<"),
                 button -> {
                     boolean newState = !menu.be.isCheckGreater();
-                    PacketDistributor.sendToServer(new UpdateSpeedSensorC2SPacket(menu.be.getBlockPos(), menu.be.getSpeedLimit(), menu.be.isEnabled(), newState));
+                    PacketDistributor.sendToServer(new UpdateSpeedSensorC2SPacket(menu.be.getBlockPos(),
+                            menu.be.getSpeedLimit(), menu.be.isEnabled(), newState));
                     button.setMessage(Component.literal(newState ? ">" : "<"));
                 },
                 menu.be.isEnabled(),
                 0xFF000000,
-                0xFF000000
-        ));
+                0xFF000000));
 
         // SPEED INPUT
         EditBox speedInput = new EditBox(this.font, this.leftPos + 60, this.topPos + 45,
@@ -71,15 +75,15 @@ public class AcceleratorSpeedSensorScreen extends AbstractContainerScreen<Accele
                 },
                 menu.be.isEnabled(),
                 0xFF93c47d,
-                0xFFe06666
-        ));
+                0xFFe06666));
     }
 
     private void onSpeedEntered(String input) {
         try {
             int value = Integer.parseInt(input);
             value = Math.max(0, Math.min(value, 20000));
-            PacketDistributor.sendToServer(new UpdateSpeedSensorC2SPacket(menu.be.getBlockPos(), value, menu.be.isEnabled(), menu.be.isCheckGreater()));
+            PacketDistributor.sendToServer(new UpdateSpeedSensorC2SPacket(menu.be.getBlockPos(), value,
+                    menu.be.isEnabled(), menu.be.isCheckGreater()));
         } catch (NumberFormatException e) {
             System.out.println(e.getMessage());
         }
@@ -99,20 +103,17 @@ public class AcceleratorSpeedSensorScreen extends AbstractContainerScreen<Accele
                 176,
                 166,
                 256,
-                256
-        );
+                256);
         renderParticleAcceleratorMessage(guiGraphics);
     }
 
-    
     private void renderParticleAcceleratorMessage(GuiGraphics guiGraphics) {
         guiGraphics.drawString(
                 this.font,
                 Component.translatable("gui.oritechthings.particle_accelerator_speed_sensor.controller"),
                 leftPos + 10,
                 topPos + 20,
-                0xFFFFFF
-        );
+                0xFFFFFF);
         if (menu.be.getTargetDesignator() == null) {
             guiGraphics.drawString(
                     this.font,
@@ -120,8 +121,7 @@ public class AcceleratorSpeedSensorScreen extends AbstractContainerScreen<Accele
                             .withStyle(style -> style.withColor(0xFF5555).withBold(true)),
                     leftPos + 100,
                     topPos + 32,
-                    0xFF5555
-            );
+                    0xFF5555);
         } else {
             BlockPos target = menu.be.getTargetDesignator();
             guiGraphics.drawString(
@@ -129,8 +129,7 @@ public class AcceleratorSpeedSensorScreen extends AbstractContainerScreen<Accele
                     Component.literal(target.toShortString()).withStyle(style -> style.withColor(0x0000FF)),
                     leftPos + 40,
                     topPos + 32,
-                    0x0000FF
-            );
+                    0x0000FF);
         }
     }
 }
